e177dcc4c119f9f8147d0d8e5c644fed3bc106ae
mintcrystaal <60029731+mintcrystaal@users.noreply.github.com>
2024-02-02 17:44:27 +0300

    Update brownian.py

--- brownian.py
+++ brownian.py
@@ -20,8 +20,8 @@ def func_curve2(x, a, b, c):
 def func_curve3(x, a, b, c, d):
 	return a * x * x * x + b * x * x + c * x + d
 
-# def func_real_exp(x, D, c, tau):
-# 	return 6 * D * x + c * np.exp(-x) + tau
+def func_real_exp(x, D, c, tau):
+	return 6 * D * x + c * np.exp(-x) + tau
 
 
 def func_real_exp(x, D, c, tau):
@@ -33,16 +33,9 @@ for t in range(0, TMAX):
 	filename1 = "dump_files/dump.gascheck." + str(t)
 	filename2 = "coords_files_n_0.25_9/coords" + str(t) + ".txt"
 
-	# file1 = open(filename1, "r")
 	file2 = open(filename2, "r")
 
 	coords_temp = []
-	# for line in file1:
-	# 	a = list(line.split())
-	# 	if (len(a) == 11):
-	# 		id1, type1, x, y, z, fx, fy, fz, vx, vy, vz = map(float, line.split())
-	# 		# coords_temp.append([x, y, z])
-
 	for line in file2:
 		x, y, z = map(float, line.split())
 		coords_temp.append([x, y, z])
@@ -109,73 +102,72 @@ plt.show()
 plt.clf()
 
 
-# # POL OF DEGREE 2
+# POL OF DEGREE 2
 
-# popt, pcov = curve_fit(func_curve2, x_for_plot, y_for_plot)
+popt, pcov = curve_fit(func_curve2, x_for_plot, y_for_plot)
 
-# perr = np.sqrt(np.diag(pcov))
-# print(popt, perr, "2")
+perr = np.sqrt(np.diag(pcov))
+print(popt, perr, "2")
 
-# plt.plot(x_for_plot, y_for_plot, 'ro', markersize=2)
+plt.plot(x_for_plot, y_for_plot, 'ro', markersize=2)
 
-# label2 = str(round(popt[0], 3)) + " * x^2 + " + str(round(popt[1], 3)) + " * x + " + str(round(popt[2], 3))
-# plt.plot(kek, func_curve2(kek, *popt), markersize=2, color="blue", label=label2)
+label2 = str(round(popt[0], 3)) + " * x^2 + " + str(round(popt[1], 3)) + " * x + " + str(round(popt[2], 3))
+plt.plot(kek, func_curve2(kek, *popt), markersize=2, color="blue", label=label2)
 
-# plt.title(r"delt_r2_avg(t)")
-# plt.ylabel("delt_r2_avg")
-# plt.xlabel(r"t")
-# plt.grid(visible=True, which='major', axis='both', alpha=1)
-# plt.grid(visible=True, which='minor', axis='both', alpha=0.5)
-# plt.legend()
-# plt.savefig("delt_r2_avg(t)2.png")
-# plt.show()
-# plt.clf()
+plt.title(r"delt_r2_avg(t)")
+plt.ylabel("delt_r2_avg")
+plt.xlabel(r"t")
+plt.grid(visible=True, which='major', axis='both', alpha=1)
+plt.grid(visible=True, which='minor', axis='both', alpha=0.5)
+plt.legend()
+plt.savefig("delt_r2_avg(t)2.png")
+plt.show()
+plt.clf()
 
 
-# # 3 DEGREE POLYNMIAL
-# plt.plot(x_for_plot, y_for_plot, 'ro', markersize=2)
+# 3 DEGREE POLYNMIAL
+plt.plot(x_for_plot, y_for_plot, 'ro', markersize=2)
 
-# popt, pcov = curve_fit(func_curve3, x_for_plot, y_for_plot)
-# perr = np.sqrt(np.diag(pcov))
-# # popt[0] = -popt[0]
-# label3 = str(round(popt[0], 3)) + " * x^3 + " + str(round(popt[1], 3)) + " * x^2 + " + str(round(popt[2], 3)) + " * x + " + str(round(popt[3], 3))
-# plt.plot(kek, func_curve3(kek, *popt), markersize=2, color="blue", label=label3)
-# print(popt, perr, "3")
-# tau = (popt[1] / popt[0]) / 3
-# print(tau, "tau")
+popt, pcov = curve_fit(func_curve3, x_for_plot, y_for_plot)
+perr = np.sqrt(np.diag(pcov))
+# popt[0] = -popt[0]
+label3 = str(round(popt[0], 3)) + " * x^3 + " + str(round(popt[1], 3)) + " * x^2 + " + str(round(popt[2], 3)) + " * x + " + str(round(popt[3], 3))
+plt.plot(kek, func_curve3(kek, *popt), markersize=2, color="blue", label=label3)
+print(popt, perr, "3")
+tau = (popt[1] / popt[0]) / 3
+print(tau, "tau")
 
-# D = (popt[2] + popt[3] / tau) / 6
+D = (popt[2] + popt[3] / tau) / 6
 
-# plt.title(r"delt_r2_avg(t)")
-# plt.ylabel("delt_r2_avg")
-# plt.xlabel(r"t")
-# plt.grid(visible=True, which='major', axis='both', alpha=1)
-# plt.grid(visible=True, which='minor', axis='both', alpha=0.5)
-# plt.legend()
-# plt.savefig("delt_r2_avg(t)3.png")
-# plt.show()
-# plt.clf()
+plt.title(r"delt_r2_avg(t)")
+plt.ylabel("delt_r2_avg")
+plt.xlabel(r"t")
+plt.grid(visible=True, which='major', axis='both', alpha=1)
+plt.grid(visible=True, which='minor', axis='both', alpha=0.5)
+plt.legend()
+plt.savefig("delt_r2_avg(t)3.png")
+plt.show()
+plt.clf()
 
 # REAL EXP
 
-# plt.plot(x_for_plot, y_for_plot, 'ro', markersize=2)
+plt.plot(x_for_plot, y_for_plot, 'ro', markersize=2)
 
-# popt, pcov = curve_fit(func_real_exp, x_for_plot, y_for_plot)
-# perr = np.sqrt(np.diag(pcov))
-# # label3 = "6 * " + str(round(popt[0], 3)) + " * t + " + str(round(popt[1], 3)) + " * exp(-t/1)" + " + " + str(round(popt[2], 3))
-# label3 = "6 * " + str(round(popt[0], 3)) + " * t + " + str(round(popt[1], 3)) + " * exp(-t/" + str(round(popt[2], 3)) + ")"
-# plt.plot(kek, func_real_exp(kek, *popt), markersize=2, color="blue", label=label3)
-# print(popt, perr, "exponent")
+popt, pcov = curve_fit(func_real_exp, x_for_plot, y_for_plot)
+perr = np.sqrt(np.diag(pcov))
+label3 = "6 * " + str(round(popt[0], 3)) + " * t + " + str(round(popt[1], 3)) + " * exp(-t/" + str(round(popt[2], 3)) + ")"
+plt.plot(kek, func_real_exp(kek, *popt), markersize=2, color="blue", label=label3)
+print(popt, perr, "exponent")
 
-# plt.title(r"delt_r2_avg(t)")
-# plt.ylabel("delt_r2_avg")
-# plt.xlabel(r"t")
-# plt.grid(visible=True, which='major', axis='both', alpha=1)
-# plt.grid(visible=True, which='minor', axis='both', alpha=0.5)
-# plt.legend()
-# plt.savefig("delt_r2_avg(t)_exp.png")
-# plt.show()
-# plt.clf()
+plt.title(r"delt_r2_avg(t)")
+plt.ylabel("delt_r2_avg")
+plt.xlabel(r"t")
+plt.grid(visible=True, which='major', axis='both', alpha=1)
+plt.grid(visible=True, which='minor', axis='both', alpha=0.5)
+plt.legend()
+plt.savefig("delt_r2_avg(t)_exp.png")
+plt.show()
+plt.clf()
 
 
 
